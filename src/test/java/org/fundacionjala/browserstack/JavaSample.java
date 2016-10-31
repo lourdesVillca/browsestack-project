e2a54aa87264affde2d3be0b0216fbf64325bd77
lourdesVillca <LourdesVillca@Fundacion-jala.local>
2016-10-31 14:23:36 -0400

    Test Browserstack

---
+++ src/test/java/org/fundacionjala/browserstack/JavaSample.java
@@ -0,0 +1,38 @@
+package org.fundacionjala.browserstack;
+
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class JavaSample {
+
+    public static final String USERNAME = "lourdesvillca1";
+    public static final String AUTOMATE_KEY = "sqxpzNKfTCqmPKkpB9mH";
+    public static final String URL = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
+
+    public static void main(String[] args) throws Exception {
+
+        DesiredCapabilities caps = new DesiredCapabilities();
+        System.getProperties().put("https.proxyHost", "172.31.90.162");
+        System.getProperties().put("https.proxyPort", "8080");
+
+        caps.setCapability("browser", "chrome");
+        caps.setCapability("browserstack.debug", "true");
+        caps.setCapability("build", "First build");
+
+        WebDriver driver = new RemoteWebDriver(new URL(URL), caps);
+        driver.get("http://www.google.com");
+        WebElement element = driver.findElement(By.name("q"));
+
+        element.sendKeys("BrowserStack");
+        element.submit();
+
+        System.out.println(driver.getTitle());
+        driver.quit();
+
+    }
+}
